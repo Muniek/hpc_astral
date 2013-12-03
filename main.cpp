14176b78d3d0490ca67713a7c286b3c1722bee25
Michał Pochopień <ramadisu@gmail.com>
2013-12-03 22:22:55 +0000

    Refactored the parallel code, now it's much cleaner

--- main.cpp
+++ main.cpp
@@ -6,86 +6,67 @@
 
 using namespace std;
 
-//zeroing the given square array
-void to_zero(vector< vector<double> > &res_a, int size) {
- for(int i = 0; i < size; i++)
-   for(int j = 0; j < size; j++)
-     res_a[i][j] = 0.0;
+void print_array(vector< vector<double> > &res_a, int rows, int columns) {
+  for(int i = 0; i < rows; i++) {
+    for(int j = 0; j < columns; j++)
+      cout << res_a[i][j] << " ";
+    cout << endl;
+  }
 }
 
-//fills given array with data get from serial iteration algorithm
-void serial(vector< vector<double> > &res_a, int size, int computations) {
-  double h = 1.0/(size - 1);
-  to_zero(res_a, size);
+void to_zero(vector< vector<double> > &res_a, int rows, int columns) {
+  for(int i = 0; i < rows; i++) {
+      for(int j =0; j < columns; j++)  {
+        res_a[i][j] = 0.0;
+      }
+    }
+}
 
-  for(int i = 0; i < size - 1; i++)
-    res_a[i][0] = pow(sin(M_PI * i * h), 2);
-
-  for(int ci = 0; ci < computations; ci++)
-    for(int i = 1; i < size - 1; i++)
-      for(int j = 1; j < size - 1; j++)
-        res_a[i][j] = 0.25 * (res_a[i - 1][j] + res_a[i + 1][j] + res_a[i][j - 1] + res_a[i][j + 1]); 
+void gauss_seidel(vector< vector<double> > &res_a, int rows, int columns) {
+  for(int i = 1; i < rows - 1; i++)
+    for(int j = 1; j < columns - 1; j++)
+      res_a[i][j] = 0.25 * (res_a[i - 1][j] + res_a[i + 1][j] + res_a[i][j - 1] + res_a[i][j + 1]); 
 }
 
 void parallel(int size, int computations) {
+  //total number of used nodes
   int nodes = MPI::COMM_WORLD.Get_size();
-  int rows_oryg = ceil((double)size/nodes);
-  int rows_halo;
+  //the number of current node
   int rank = MPI::COMM_WORLD.Get_rank();
-  vector< vector<double> > res_a;
-
-  //first node
-  if (rank == 0) {
-    rows_halo = 1;
-    res_a.resize(rows_oryg + rows_halo, vector<double>(size));
-  //last node
-  } else if(rank == nodes-1) {
-    rows_oryg = size - rows_oryg * (nodes-1);
-    rows_halo = 1;
-    res_a.resize((rows_oryg + rows_halo), vector<double>(size));
-  } else {//central nodes
-    rows_halo = 2;
-    res_a.resize(rows_oryg + rows_halo, vector<double>(size));
-  }
-  
-  int rows_eff = rows_oryg + rows_halo;
-
-  //zeroing the array
-  for(int i = 0; i < rows_eff; i++) {
-    for(int j =0; j < size; j++)  {
-      res_a[i][j] = 0.0;
-    }
-  }
-
+  //the value of step
   double h = 1.0/(size - 1);
+  //setting the boolean helper - true for last node 
+  bool isLast = false;
+  if (rank == nodes - 1) isLast = true;
+  //the proper amount of rows for computing for current node
+  //the -2 is because in whole computed array the first and last rows equals 0s (that's the condition)
+  int comp_rows = ceil((double)(size - 2) / nodes); 
+  //this amount if different for last row - it's just what's left
+  if (isLast)
+    comp_rows = (size - 2) - comp_rows * (nodes - 1); 
+  
+  //setting up the array
+  //to comp_rows there are 2 extra (halo) ones,
+  //where for first and last node, they're just an edges instead of being halo ones
+  //the std::vector is default initialied with 0s
+  int all_rows = comp_rows + 2;
+  vector< vector<double> > res_a(all_rows, vector<double>(size));
 
-  //filling the x = 0 column with primary data
-  for(int i = 0; i < rows_eff; i++)
-    if (rank == 0)
-      res_a[i][0] = pow(sin(M_PI * i * h), 2); 
-    else
-      res_a[i][0] = pow(sin(M_PI * h * (i + rank * rows_oryg - 1)), 2);
-
-  //gauss-seidel iterations
+  //filling the x = 0 column with proper data - sin^2(PI * y) 
+  for(int i = 0; i < all_rows; i++)
+    res_a[i][0] = pow(sin(M_PI * h * (i + (rank * (ceil((double)(size - 2) / nodes))))), 2);  
+ 
   for(int ci = 0; ci < computations; ci++)
-    for(int i = 1; i < rows_oryg; i++)
-      for(int j = 1; j < size - 1; j++)
-        res_a[i][j] = 0.25 * (res_a[i - 1][j] + res_a[i + 1][j] + res_a[i][j - 1] + res_a[i][j + 1]); 
+    gauss_seidel(res_a, all_rows, size);
 
-
+  //print the array (test)
   cout << setprecision(3);
   const int m = 1;
   if (rank == 0) {
-    //printing the array
-     
-    for(int i = 0; i < rows_eff; i++) {
-      for(int j = 0; j < size; j++)
-        cout << res_a[i][j] << " ";
-      cout << endl;
-    }
-    //sending message
+    print_array(res_a, all_rows, size);
+    cout << endl;
     MPI::COMM_WORLD.Send(&m, 1, MPI_INT, (rank+1), 0);  
-} else {
+  } else {
     int recv = 0;
     MPI::Status status;
     MPI::COMM_WORLD.Recv(&recv, 1, MPI_INT, rank-1, MPI::ANY_TAG, status); 
@@ -93,15 +74,11 @@ void parallel(int size, int computations) {
     while (!recv) { }
 
     if (recv) {
-      //printing the array
-      for(int i = 0; i < rows_eff; i++) {
-        for(int j = 0; j < size; j++)
-          cout << res_a[i][j] << " ";
-        cout << endl;
-      }
+      print_array(res_a, all_rows, size);
       if (rank != (nodes -1)) { 
         MPI::COMM_WORLD.Send(&m, 1, MPI_INT, (rank+1), 0);  
       }
+      cout << endl;
     }
   }
 }
@@ -109,7 +86,7 @@ void parallel(int size, int computations) {
 //fills given array with data get from analytical algorithm
 void analytical(vector< vector<double> > &res_a, int size, int computations) {
   double h = 1.0/(size - 1);
-  to_zero(res_a, size);
+  to_zero(res_a, size, size);
 
   for(int i = 0; i < size - 1; i++) {
     for(int j = 0; j < size - 1; j++) {
@@ -143,7 +120,7 @@ double average_error(vector< vector<double> > &analytical_a, vector< vector<doub
 
 main(int argc, char *argv[])  {
   MPI::Init(argc, argv);
-  int n = 9;
+  int n = 12;
   int computations = 100;
   parallel(n, computations);
   MPI::Finalize();
